ce54c4085d2186bdab420bcefa059dfcd8b9b374
l1nway <l1nway@icloud.com>
2026-09-19 04:22:57 +0200

    added redirecting to apple app

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type {NextConfig} from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [{key: 'Content-Type', value: 'application/json'}],
+      },
+    ]
+  },
+}
 
 export default nextConfig
